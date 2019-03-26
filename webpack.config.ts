9d48150e6b42e03f549e8d63dd56ff5da0676bc7
Charles Samborski <demurgos@demurgos.net>
2019-03-26 17:06:00 +0100

    Split test samples into multiple pages

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,61 @@
-import webpackMerge = require("webpack-merge");
-import { getBrowserPartial } from "./webpack/browser.webpack";
-import { getCommonPartial } from "./webpack/common.webpack";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import sysPath from "path";
+import ScriptExtPlugin from "script-ext-html-webpack-plugin";
+import webpack from "webpack";
 
-export function getWebpackConfiguration(options: any, webpackOptions: any): any {
-  options = options || {};
+const PROJECT_ROOT: string = __dirname;
 
-  const configs: any = {};
-
-  const isProduction: boolean = Boolean(options.production);
-
-  configs.browser = webpackMerge(
-    // {mode: "development"},
-    getCommonPartial(isProduction),
-    getBrowserPartial(isProduction),
-  );
-
-  const builds: any = [];
-  builds.push(configs.browser);
-
-  return builds;
-}
+const config: webpack.Configuration = {
+  entry: [
+    sysPath.resolve(PROJECT_ROOT, "src", "main.browser.ts"),
+  ],
+  output: {
+    path: sysPath.resolve(PROJECT_ROOT, "build", "browser", "scripts"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".js", ".json", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+        options: {
+          configFile: "browser.tsconfig.json",
+          onlyCompileBundledFiles: true,
+          // visualStudioErrorFormat: true,
+          compilerOptions: {
+            declaration: false,
+          },
+        },
+      },
+    ],
+  },
+  target: "web",
+  plugins: [
+    ...["index", "homestuck-beta", "squares", "morph", "shumway-3"].map((name: string): webpack.Plugin => {
+      return new HtmlWebpackPlugin({
+        template: sysPath.resolve(PROJECT_ROOT, "src", "static", `${name}.html`),
+        filename: sysPath.resolve(PROJECT_ROOT, "build", "browser", `${name}.html`),
+        inject: "head",
+      });
+    }),
+    new ScriptExtPlugin({
+      defaultAttribute: "async",
+    }) as webpack.Plugin,
+    new CopyWebpackPlugin([
+      {
+        from: sysPath.resolve(PROJECT_ROOT, "src", "static", "**", "*.{gif,ico,jpg,png,svg,swf,css,js}"),
+        to: sysPath.resolve(PROJECT_ROOT, "build", "browser"),
+        context: sysPath.resolve(PROJECT_ROOT, "src", "static"),
+        ignore: ["index.html"],
+      },
+    ]),
+  ],
+};
 
 // tslint:disable-next-line:no-default-export
-export default getWebpackConfiguration;
+export default config;
